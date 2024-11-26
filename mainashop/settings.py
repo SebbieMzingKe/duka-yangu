bf6bb70e25c4aa38aa6f3856c27a81f3e7279e38
SebbieMzingKe <sebbievilar2@gmail.com>
2024-11-26 18:56:34 +0300

    Update (feat): add items, products to cart, modify products quantity in the cart

--- mainashop/settings.py
+++ mainashop/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
 ]
+
+
+
+CART_SESSION_ID = 'cart'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
